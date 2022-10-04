a0662cd4efe98666afeacd6fd6d5b42fc5daba49
Ohiboi <ndr.baltera@gmail.com>
2022-10-04 14:54:10 +0200

    add readme

---
+++ README.md
@@ -0,0 +1,4 @@
+# My First Node Project
+## This is the first node project for Lacerba
+
+To srun the program, use "ts-node main.ts"
---
+++ dist/main.js
@@ -0,0 +1,11 @@
+"use strict";
+console.log('Il mio primo programmino');
+let myvar = 10;
+myvar += 40;
+console.log('myvar =', myvar);
+let miaInterfaccia = {
+    mioNumero: 40,
+    miaStringa: 'quaranta',
+};
+console.log('mioNumero =', miaInterfaccia.mioNumero);
+console.log('miaStringa =', miaInterfaccia.miaStringa);
